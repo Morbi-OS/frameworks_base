bionic-linux/android_frameworks_base

Rename method isConnectable()
LocalBluetoothProfile.isConnectable() checks whether the user can initiate a connection
for a specific profile, not really whether the profile is connectable.

Change-Id: If6c6cd1554acf35db2460ea6ddb65148a7e86e45
Bug: 79982487
Test: build

packages/SettingsLib/src/com/android/settingslib/bluetooth/A2dpSinkProfile.java
@@ -99,7 +99,7 @@ final class A2dpSinkProfile implements LocalBluetoothProfile {
                 BluetoothProfile.A2DP_SINK);
     }
 
-    public boolean isConnectable() {
+    public boolean shouldDisplayEntry() {
         return true;
     }
 

packages/SettingsLib/src/com/android/settingslib/bluetooth/HeadsetProfile.java
@@ -106,7 +106,7 @@ public class HeadsetProfile implements LocalBluetoothProfile {
                 BluetoothProfile.HEADSET);
     }
 
-    public boolean isConnectable() {
+    public boolean shouldDisplayEntry() {
         return true;
     }
 

packages/SettingsLib/src/com/android/settingslib/bluetooth/HearingAidProfile.java
@@ -103,7 +103,7 @@ public class HearingAidProfile implements LocalBluetoothProfile {
                 BluetoothProfile.HEARING_AID);
     }
 
-    public boolean isConnectable() {
+    public boolean shouldDisplayEntry() {
         return false;
     }
 

packages/SettingsLib/src/com/android/settingslib/bluetooth/HfpClientProfile.java
@@ -107,7 +107,7 @@ final class HfpClientProfile implements LocalBluetoothProfile {
     }
 
     @Override
-    public boolean isConnectable() {
+    public boolean shouldDisplayEntry() {
         return true;
     }
 

packages/SettingsLib/src/com/android/settingslib/bluetooth/HidDeviceProfile.java
@@ -102,7 +102,7 @@ public class HidDeviceProfile implements LocalBluetoothProfile {
     }
 
     @Override
-    public boolean isConnectable() {
+    public boolean shouldDisplayEntry() {
         return true;
     }
 

packages/SettingsLib/src/com/android/settingslib/bluetooth/LocalBluetoothProfile.java
@@ -26,9 +26,9 @@ import android.bluetooth.BluetoothDevice;
 public interface LocalBluetoothProfile {
 
     /**
-     * Returns true if the user can initiate a connection, false otherwise.
+     * Return {@code true} if the user can initiate a connection for this profile in UI.
      */
-    boolean isConnectable();
+    boolean shouldDisplayEntry();
 
     /**
      * Returns true if the user can enable auto connection for this profile.

packages/SettingsLib/src/com/android/settingslib/bluetooth/PbapServerProfile.java
@@ -80,7 +80,7 @@ public class PbapServerProfile implements LocalBluetoothProfile {
         BluetoothPbap pbap = new BluetoothPbap(context, new PbapServiceListener());
     }
 
-    public boolean isConnectable() {
+    public boolean shouldDisplayEntry() {
         return true;
     }
 

packages/SettingsLib/src/com/android/settingslib/bluetooth/SapProfile.java
@@ -106,7 +106,7 @@ final class SapProfile implements LocalBluetoothProfile {
                 BluetoothProfile.SAP);
     }
 
-    public boolean isConnectable() {
+    public boolean shouldDisplayEntry() {
         return true;
     }
 

Morbi-OS/frameworks_base

packages/SettingsLib/src/com/android/settingslib/bluetooth/A2dpSinkProfile.java
@@ -99,7 +99,7 @@ final class A2dpSinkProfile implements LocalBluetoothProfile {
                 BluetoothProfile.A2DP_SINK);
     }
 
-    public boolean isConnectable() {
+    public boolean accessProfileEnabled() {
         return true;
     }
 

packages/SettingsLib/src/com/android/settingslib/bluetooth/HeadsetProfile.java
@@ -106,7 +106,7 @@ public class HeadsetProfile implements LocalBluetoothProfile {
                 BluetoothProfile.HEADSET);
     }
 
-    public boolean isConnectable() {
+    public boolean accessProfileEnabled() {
         return true;
     }
 

packages/SettingsLib/src/com/android/settingslib/bluetooth/HearingAidProfile.java
@@ -103,7 +103,7 @@ public class HearingAidProfile implements LocalBluetoothProfile {
                 BluetoothProfile.HEARING_AID);
     }
 
-    public boolean isConnectable() {
+    public boolean accessProfileEnabled() {
         return false;
     }
 

packages/SettingsLib/src/com/android/settingslib/bluetooth/HfpClientProfile.java
@@ -107,7 +107,7 @@ final class HfpClientProfile implements LocalBluetoothProfile {
     }
 
     @Override
-    public boolean isConnectable() {
+    public boolean accessProfileEnabled() {
         return true;
     }
 

packages/SettingsLib/src/com/android/settingslib/bluetooth/HidDeviceProfile.java
@@ -102,7 +102,7 @@ public class HidDeviceProfile implements LocalBluetoothProfile {
     }
 
     @Override
-    public boolean isConnectable() {
+    public boolean accessProfileEnabled() {
         return true;
     }
 

packages/SettingsLib/src/com/android/settingslib/bluetooth/LocalBluetoothProfile.java
@@ -26,9 +26,9 @@ import android.bluetooth.BluetoothDevice;
 public interface LocalBluetoothProfile {
 
     /**
-     * Returns true if the user can initiate a connection, false otherwise.
+     * Return {@code true} if the user can initiate a connection for this profile in UI.
      */
-    boolean isConnectable();
+    boolean accessProfileEnabled();
 
     /**
      * Returns true if the user can enable auto connection for this profile.

packages/SettingsLib/src/com/android/settingslib/bluetooth/PbapServerProfile.java
@@ -80,7 +80,7 @@ public class PbapServerProfile implements LocalBluetoothProfile {
         BluetoothPbap pbap = new BluetoothPbap(context, new PbapServiceListener());
     }
 
-    public boolean isConnectable() {
+    public boolean accessProfileEnabled() {
         return true;
     }
 

packages/SettingsLib/src/com/android/settingslib/bluetooth/SapProfile.java
@@ -106,7 +106,7 @@ final class SapProfile implements LocalBluetoothProfile {
                 BluetoothProfile.SAP);
     }
 
-    public boolean isConnectable() {
+    public boolean accessProfileEnabled() {
         return true;
     }
 