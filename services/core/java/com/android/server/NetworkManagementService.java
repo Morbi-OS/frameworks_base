Neoteric-OS/android_frameworks_base

Split updateMeteredNetwork{Allow, Deny}List into add and remove
Bug: 218494748
Test: atest FrameworksNetTests
Change-Id: I06fdf88a6b6988377478665389f307ea7269bbf1

services/core/java/com/android/server/NetworkManagementService.java
@@ -1166,9 +1166,17 @@ public class NetworkManagementService extends INetworkManagementService.Stub {
             final ConnectivityManager cm = mContext.getSystemService(ConnectivityManager.class);
             try {
                 if (allowlist) {
-                    cm.updateMeteredNetworkAllowList(uid, enable);
+                    if (enable) {
+                        cm.addMeteredNetworkAllowList(uid);
+                    } else {
+                        cm.removeMeteredNetworkAllowList(uid);
+                    }
                 } else {
-                    cm.updateMeteredNetworkDenyList(uid, enable);
+                    if (enable) {
+                        cm.addMeteredNetworkDenyList(uid);
+                    } else {
+                        cm.removeMeteredNetworkDenyList(uid);
+                    }
                 }
                 synchronized (mRulesLock) {
                     if (enable) {

Morbi-OS/frameworks_base

services/core/java/com/android/server/NetworkManagementService.java
@@ -1166,9 +1166,17 @@ public class NetworkManagementService extends INetworkManagementService.Stub {
             final ConnectivityManager cm = mContext.getSystemService(ConnectivityManager.class);
             try {
                 if (allowlist) {
-                    cm.updateMeteredNetworkAllowList(uid, enable);
+                    if (enable) {
+                        cm.addUidToMeteredNetworkAllowList(uid);
+                    } else {
+                        cm.removeUidFromMeteredNetworkAllowList(uid);
+                    }
                 } else {
-                    cm.updateMeteredNetworkDenyList(uid, enable);
+                    if (enable) {
+                        cm.addUidToMeteredNetworkDenyList(uid);
+                    } else {
+                        cm.removeUidFromMeteredNetworkDenyList(uid);
+                    }
                 }
                 synchronized (mRulesLock) {
                     if (enable) {