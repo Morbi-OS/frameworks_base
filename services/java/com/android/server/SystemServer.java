d987e5645d4b902e68ff6a472db2b80721353302
SamarV-121 <samar@samarv121.dev>
2025-05-09 00:40:05 +0530

    PropImitationHooks: Dynamically spoof props for GMS
    neobuddy89: Adapted for PixelPropsUtils. Use local props as fallback.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Ritesh <SahanyRitesh@proton.me>
    Signed-off-by: texascake <tiktodz@soulvibe-team.cloud>

--- services/java/com/android/server/SystemServer.java
+++ services/java/com/android/server/SystemServer.java
@@ -149,6 +149,7 @@ import com.android.server.blob.BlobStoreManagerService;
 import com.android.server.broadcastradio.BroadcastRadioService;
 import com.android.server.camera.CameraServiceProxy;
 import com.android.server.clipboard.ClipboardService;
+import com.android.server.afterlife.AttestationService;
 import com.android.server.companion.CompanionDeviceManagerService;
 import com.android.server.companion.virtual.VirtualDeviceManagerService;
 import com.android.server.compat.PlatformCompat;
@@ -2768,6 +2769,11 @@ public final class SystemServer implements Dumpable {
             // AfterlifeDeviceConfigService
             t.traceBegin("AfterlifeDeviceConfigService");
             mSystemServiceManager.startService(AfterlifeDeviceConfigService.class);
+            t.traceEnd();
+
+            // AttestationService
+            t.traceBegin("AttestationService");
+            mSystemServiceManager.startService(AttestationService.class);
             t.traceEnd();
 
             // LineageHardware
